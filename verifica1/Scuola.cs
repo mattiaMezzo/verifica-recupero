7754b3ef64731a6550745e7668bf589114ed5e11
Mattia Mezzogori <MATTIA.MEZZOGORI@local.ittcesena.it>
2023-03-18 08:59:26 +0100

    versione 1.1 aggiunta acquisizione varriabili

--- verifica1/Scuola.cs
+++ verifica1/Scuola.cs
@@ -8,5 +8,22 @@ namespace verifica1
 {
     internal class Scuola
     {
-    }
+        internal class Scuola
+        {
+            string nome;
+            string indirizzo;
+            string apertura;
+            string chiusura;
+
+            List<alunno>
+            alunni = new List<alunno>();
+
+            Scuola(string nome, string indirizzo, string apertura, string chiusura)
+            {
+                this.nome = nome;
+                this.indirizzo = indirizzo;
+                this.apertura = apertura;
+                this.chiusura = chiusura;
+            }
+        }
 }
